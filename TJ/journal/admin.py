39dd81f0c19fb9fd09ea3e89feeb4e7e729d165c
TJ <tseth888@gmail.com>
2025-05-11 01:12:38 +0000

    added a functionality for admin only to add profile photo at the backend

--- TJ/journal/admin.py
+++ TJ/journal/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 #here we import our thought model in the model.py file into our admin.py file
-from .models import Thought
+from .models import Thought, Profile
 
 #here we register our thought model inside our admin page
 admin.site.register(Thought)
+
+admin.site.register(Profile)
